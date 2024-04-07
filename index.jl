efd12543edcccd73ec94ecaa3309e4c65f7979ed
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-04-07 12:28:27 +0200

    Update index.jl

--- index.jl
+++ index.jl
@@ -3,42 +3,43 @@ using MultiDocumenter
 clonedir = mktempdir()
 
 docs = [
-    ("JuliaVlasov/GEMPIC.jl.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+    MultiDocumenter.MultiDocRef(
         upstream = joinpath(clonedir, "GEMPIC"),
         path = "gempic",
-        name = "GEMPIC"
+        name = "GEMPIC",
+        giturl = "https://github.com/JuliaVlasov/GEMPIC.jl.git",
     ),
-    ("JuliaVlasov/SemiLagrangian.jl.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+    MultiDocumenter.MultiDocRef(
         upstream = joinpath(clonedir, "SemiLagrangian"),
-        path = "semi-lagrangian",
-        name = "SemiLagrangian"
+        path = "semilagrangian",
+        name = "SemiLagrangian",
+        giturl = "https://github.com/JuliaVlasov/SemiLagrangian.jl.git",
     ),
-    ("JuliaVlasov/VlasovSolvers.jl.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+    MultiDocumenter.MultiDocRef(
         upstream = joinpath(clonedir, "VlasovSolvers"),
-        path = "vlasov-solvers",
-        name = "VlasovSolvers"
+        path = "vlasovsolvers",
+        name = "VlasovSolvers",
+        giturl = "https://github.com/JuliaVlasov/VlasovSolvers.jl.git",
     ),
-    ("JuliaVlasov/SpinGEMPIC.jl.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+    MultiDocumenter.MultiDocRef(
         upstream = joinpath(clonedir, "SpinGEMPIC"),
-        path = "scalar-spin",
-        name = "SpinGEMPIC"
+        path = "scalarspin",
+        name = "SpinGEMPIC",
+        giturl = "https://github.com/JuliaVlasov/SpinGEMPIC.jl.git",
     ),
-    ("JuliaVlasov/VectorSpin.jl.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+    MultiDocumenter.MultiDocRef(
         upstream = joinpath(clonedir, "VectorSpin"),
-        path = "vector-spin",
-        name = "VectorSpin"
+        path = "vectorspin",
+        name = "VectorSpin",
+        giturl = "https://github.com/JuliaVlasov/VectorSpin.jl.git",
     ),
 ]
-
-for ((remote, branch), docref) in docs
-    run(`git clone --depth 1 https://github.com/$remote --branch $branch --single-branch $(docref.upstream)`)
-end
 
 outpath = joinpath(@__DIR__, "out")
 
 MultiDocumenter.make(
     outpath,
-    collect(last.(docs));
+    docs;
     search_engine = MultiDocumenter.SearchConfig(
         index_versions = ["dev"],
         engine = MultiDocumenter.FlexSearch
